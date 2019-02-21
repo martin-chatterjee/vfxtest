f2fb9f720eaf5f9d0865036cbb682d7327f9e9f4
Martin Chatterjee <martin@chatterjee.de>
2019-02-21 18:11:57 +0100

    updated .gitignore, first skeleton setup.py to reserve name on PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='vfxtest',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='',
+    author='Martin Chatterjee',
+    author_email='martin@chatterjee.de',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
