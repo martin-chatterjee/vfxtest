6a2cf1c39b57e5f9bb82f112b07f18b68da8b93b
Martin Chatterjee <martin@chatterjee.de>
2022-12-01 17:21:04 +0100

    Update copyright header years.

--- vfxtest.py
+++ vfxtest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2019-2021, Martin Chatterjee. All rights reserved.
+# Copyright (c) 2019-2022, Martin Chatterjee. All rights reserved.
 # Licensed under MIT License (--> LICENSE.txt)
 # -----------------------------------------------------------------------------
 
